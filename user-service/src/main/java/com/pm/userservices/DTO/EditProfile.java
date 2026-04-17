21af9d64e8a6a9adcc67092d8943154e05cb2aae
prashantdubeypng <vinoddubey6059@gmail.com>
2026-04-18 01:28:57 +0530

    fix:loadbalancing

---
+++ user-service/src/main/java/com/pm/userservices/DTO/EditProfile.java
@@ -0,0 +1,10 @@
+package com.pm.userservices.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EditProfile {
+    private String name;
+}
--- user-service/src/main/java/com/pm/userservices/Services/UserService.java
+++ user-service/src/main/java/com/pm/userservices/Services/UserService.java
@@ -1,10 +1,13 @@
 package com.pm.userservices.Services;
 
+import com.pm.userservices.DTO.AuthResponse;
+import com.pm.userservices.DTO.ProfileRequest;
 import com.pm.userservices.DTO.Signup;
 import com.pm.userservices.Model.User;
 import com.pm.userservices.Reprositery.RefreshTokenReposiotory;
 import com.pm.userservices.Reprositery.UserRepostiory;
 import com.pm.userservices.config.security.SecurityConfig;
+import com.pm.userservices.security.JwtUtility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final JwtUtility jwtUtil;
     private final UserRepostiory userrepo;
     private final RefreshTokenReposiotory refreshtokenrepo;
     private final SecurityConfig securityconfig;
-// need to implement the bloom filter
+//TODO: need to implement the bloom filter
+//    for now we are using the indexing -> we are fully depened upon the db to verify is the username is taken or not
 
     public User createUser(Signup signup){
         User user = User.builder()
@@ -41,6 +46,19 @@ public class UserService {
             }
 
             throw new RuntimeException("Database error");
+        }
+    }
+    public ProfileRequest profile(String token){
+        String Username = jwtUtil.extractUsername(token);
+        User data = userrepo.findByUsername(Username).orElseThrow(()-> new RuntimeException(
+                "username is incorrect"
+        ));
+        return new ProfileRequest(data.getUsername(), data.getEmail(), data.getName());
+    }
+    public void EditProfile(String name , String token){
+        int res = userrepo.updateNameByUsername(token , name);
+        if(res == 0){
+            throw new RuntimeException("User Not Found");
         }
     }
 }
